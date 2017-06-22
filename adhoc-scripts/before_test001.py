c37a316d802fe0b31ce5b1a9e3e1866ddfd0b0aa
Himanshu Roy <hroy@redhat.com>
2017-06-22 18:28:10 +0530

    fix

--- adhoc-scripts/before_test001.py
+++ adhoc-scripts/before_test001.py
@@ -6,9 +6,11 @@ import subprocess
 stream = open("cfme-performance/conf/cfme_performance.yml", "r")
 confyml = yaml.load(stream)
 
-for scenario in list(confyml['tests']['workloads']['test_refresh_vms']['scenarios']):
-    if not 'rhos7' in scenario:
-        del confyml['tests']['workloads']['test_refresh_vms']['scenarios'][scenario]
+i = 0
+while i < len(confyml['tests']['workloads']['test_refresh_vms']['scenarios']):
+    if not 'rhos7' in confyml['tests']['workloads']['test_refresh_vms']['scenarios'][i]['name']:
+        del confyml['tests']['workloads']['test_refresh_vms']['scenarios'][i]
+    i = i + 1
 
 with open("cfme-performance/conf/cfme_performance.yml", "w") as outfile:
     yaml.dump(confyml, outfile, default_flow_style=False)
