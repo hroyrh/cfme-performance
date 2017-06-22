f10cdaddc958d07679841d8a856f460ad484112f
Himanshu Roy <hroy@redhat.com>
2017-06-22 14:54:11 +0530

    fix

--- adhoc-scripts/before_test001.py
+++ adhoc-scripts/before_test001.py
@@ -7,7 +7,7 @@ stream = open("cfme-performance/conf/cfme_performance.yml", "r")
 confyml = yaml.load(stream)
 
 for scenario in confyml['tests']['workloads']['test_refresh_vms']:
-    if not 'rhevm' in scenario:
+    if not 'rhos7' in scenario:
         del confyml['tests']['workloads']['test_refresh_vms'][scenario]
 
 confile = open("cfme-performance/conf/cfme_performance.yml", "w")
