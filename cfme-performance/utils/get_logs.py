636462d5055c2e2b13d5e2b967fb32c8edc2d00e
Himanshu Roy <hroy@redhat.com>
2017-06-22 16:39:30 +0530

    collecting all logs , system and vmdb for with this

--- cfme-performance/utils/get_logs.py
+++ cfme-performance/utils/get_logs.py
@@ -5,6 +5,8 @@ import subprocess
 
 def collect_logs():
     logger.info('Starting evm log collection')
-    command_str = "scp root@" + cfme_performance['appliance']['ip_address'] + ":/var/www/miq/vmdb/log/evm.log* cfme-performance/log/"
+    command_str = "mkdir -p cfme-performance/log/vmdb-logs/ ; scp root@" + cfme_performance['appliance']['ip_address'] + ":/var/www/miq/vmdb/log/* cfme-performance/log/vmdb-logs/"
+    print subprocess.Popen(command_str, shell=True, stdout=subprocess.PIPE).stdout.read()
+    command_str = "mkdir -p cfme-performance/log/system-logs/ ; scp root@" + cfme_performance['appliance']['ip_address'] + ":/var/log/* cfme-performance/log/system-logs/"
     print subprocess.Popen(command_str, shell=True, stdout=subprocess.PIPE).stdout.read()
     logger.info('evm log collection Finished')
