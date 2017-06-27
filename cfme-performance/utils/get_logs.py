d4573ac5a8bca7b5a71f1bdf440f57393f093a6e
Himanshu Roy <hroy@redhat.com>
2017-06-27 11:00:06 +0530

    minor fix

--- cfme-performance/utils/get_logs.py
+++ cfme-performance/utils/get_logs.py
@@ -5,8 +5,17 @@ import subprocess
 
 def collect_logs():
     logger.info('Starting evm log collection')
-    command_str = "mkdir -p cfme-performance/log/vmdb-logs/ ; scp root@" + cfme_performance['appliance']['ip_address'] + ":/var/www/miq/vmdb/log/* cfme-performance/log/vmdb-logs/"
-    print subprocess.Popen(command_str, shell=True, stdout=subprocess.PIPE).stdout.read()
-    command_str = "mkdir -p cfme-performance/log/system-logs/ ; scp root@" + cfme_performance['appliance']['ip_address'] + ":/var/log/* cfme-performance/log/system-logs/"
-    print subprocess.Popen(command_str, shell=True, stdout=subprocess.PIPE).stdout.read()
+    subprocess.check_call("mkdir", "-p", "cfme-performance/log/vmdb-logs/" ,shell=True)
+    logstr = "root@" + cfme_performance['appliance']['ip_address'] + ":/var/www/miq/vmdb/log/*"
+    subprocess.check_call("scp", logstr, "cfme-performance/log/vmdb-logs/" ,shell=True)
+
+    subprocess.check_call("mkdir", "-p", "cfme-performance/log/system-logs/" ,shell=True)
+    logstr = "root@" + cfme_performance['appliance']['ip_address'] + ":/var/log/*"
+    subprocess.check_call("scp", logstr, "cfme-performance/log/system-logs/" ,shell=True)
+
     logger.info('evm log collection Finished')
+    #command_str = "mkdir -p cfme-performance/log/vmdb-logs/ ; scp root@" + cfme_performance['appliance']['ip_address'] + ":/var/www/miq/vmdb/log/* cfme-performance/log/vmdb-logs/"
+    #print subprocess.Popen(command_str, shell=True, stdout=subprocess.PIPE).stdout.read()
+    #command_str = "mkdir -p cfme-performance/log/system-logs/ ; scp root@" + cfme_performance['appliance']['ip_address'] + ":/var/log/* cfme-performance/log/system-logs/"
+    #print subprocess.Popen(command_str, shell=True, stdout=subprocess.PIPE).stdout.read()
+    #logger.info('evm log collection Finished')
