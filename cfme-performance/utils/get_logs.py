9b703aa65f367abf2e7e38162da5a81731dbfe60
Himanshu Roy <hroy@redhat.com>
2017-06-27 14:29:23 +0530

    5 min pause added at the end

--- cfme-performance/utils/get_logs.py
+++ cfme-performance/utils/get_logs.py
@@ -13,6 +13,8 @@ def collect_logs():
     logstr = "root@" + cfme_performance['appliance']['ip_address'] + ":/var/log/*"
     subprocess.check_call(["scp", logstr, "cfme-performance/log/system-logs/"])
 
+    subprocess.check_call(["sleep", "300"])
+
     logger.info('evm log collection Finished')
     #command_str = "mkdir -p cfme-performance/log/vmdb-logs/ ; scp root@" + cfme_performance['appliance']['ip_address'] + ":/var/www/miq/vmdb/log/* cfme-performance/log/vmdb-logs/"
     #print subprocess.Popen(command_str, shell=True, stdout=subprocess.PIPE).stdout.read()
